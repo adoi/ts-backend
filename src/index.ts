149725580556f3136a4e7156041e87a3920985c9
adoi <adonismurati@gmail.com>
2018-11-12 08:41:35 +0100

    Update app to use Apollo Server

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import * as express from 'express';
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs } from "./typeDefs";
+import { resolvers } from "./resolvers";
+import { createConnection } from "typeorm";
 
-createConnection().then(async connection => {
+const startServer = async () => {
+    const server = new ApolloServer({ typeDefs, resolvers });
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-    
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-     
-    console.log("Here you can setup and run express/koa/any other framework.");
-    
-}).catch(error => console.log(error));
+    await createConnection();
+
+    const app = express();
+
+    server.applyMiddleware({ app });
+
+    app.listen({ port: 4000 }, () =>
+        console.log(`🚀  Server ready at ${server.graphqlPath}`)
+    );
+};
+
+startServer();
